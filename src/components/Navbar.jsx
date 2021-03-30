1f6e8eb5cce0a3f028d611438f8d749abd8718d3
Stephen-Roach <stephenroach21@gmail.com>
2021-03-30 18:19:45 -0500

    added logo to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+const navbarImg = process.env.PUBLIC_URL + '/images/';
 
 function Navbar() {
   return (
     <div>
+      <img src={`${navbarImg}jobbyist-logo.png`} alt='logo' />
       <h1>This is a navbar!</h1>
     </div>
   );
