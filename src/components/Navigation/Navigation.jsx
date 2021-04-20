783b41689bbd063fb5f96c9e6e9a093a97acc96c
Stephen-Roach <stephenroach21@gmail.com>
2021-04-19 22:37:01 -0500

    added navbar logo

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import { Navbar, Form, FormControl, Button, Nav } from 'react-bootstrap';
-// import Navbar from 'react-bootstrap/Navbar';
-// import Form from 'react-bootstrap/Form';
-// import FormControl from 'react-bootstrap/FormControl';
-// import Button from 'react-bootstrap/Button';
-// import Nav from 'react-bootstrap/Nav';
-import './Nav.css';
-// const navbarImg = process.env.PUBLIC_URL + '/images/';
+import './Navigation.css';
+const navbarImg = process.env.PUBLIC_URL + '/images/';
 
 function Navigation() {
   return (
@@ -20,7 +15,14 @@ function Navigation() {
     // </div>
     <>
       <Navbar bg='dark' variant='dark'>
-        <Navbar.Brand href='#home'>Navbar</Navbar.Brand>
+        <Navbar.Brand href='#home'>
+          {' '}
+          <img
+            className='navbar__Logo'
+            src={`${navbarImg}jobbyist-logo.png`}
+            alt='logo'
+          />
+        </Navbar.Brand>
         <Nav className='mr-auto'>
           <Nav.Link href='#home'>Home</Nav.Link>
           <Nav.Link href='#features'>Features</Nav.Link>
